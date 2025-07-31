6374d6c41f1696757e17f75136f8d02737943570
Vova Sharaienko <sharaienko@google.com>
2025-07-31 01:25:55 +0000

    PixelAtomsTest: added support to run without device
    - re-structed test folder for PixelAtomsTest to be aligned
      with ownership
    
    Flag: EXEMPT TEST_ONLY
    Bug: 434254746
    Test: atest
    Change-Id: Id6be8a4b0e174fb63274579c2e63f100a717506d

--- pixelstats/test/PixelAtomsTest/android/hardware/google/pixel/BasePixelAtomsTest.java
+++ pixelstats/test/PixelAtomsTest/android/hardware/google/pixel/BasePixelAtomsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2022 Google LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ public abstract class BasePixelAtomsTest {
                         .that(message.findFieldByNumber(1).getName())
                         .isEqualTo("reverse_domain_name");
                 assertWithMessage(atomName + " has more than 127 fields.")
-                        .that(field.getMessageType().getFields().size())
+                        .that(message.getFields().size())
                         .isLessThan(128);
             }
         }
